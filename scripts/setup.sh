5ccfc2bbd09a3d411be82e940a4d21a93ae2b562
yoyoyousei <yoyoyousei2016@gmail.com>
2018-03-19 17:44:07 +0900

    init

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server
+
+
